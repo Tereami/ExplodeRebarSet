e1b9af2b6ec8a28ffb60b64fafa1d3c3967cfa86
Tereami <undress.my.future@gmail.com>
2021-04-13 23:56:34 +0300

    Add project files.

---
+++ ExplodeRebarSet/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using System.IO;
+
+namespace ExplodeRebarSet
+{
+    public class App : IExternalApplication
+    {
+        public static string assemblyPath = "";
+        public static string iconsPath = "";
+        public static string username = "";
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            assemblyPath = typeof(App).Assembly.Location;
+            iconsPath = Path.GetDirectoryName(assemblyPath);
+
+            string tabName = "Weandrevit";
+            try { application.CreateRibbonTab(tabName); }
+            catch { }
+
+            RibbonPanel panel = null;
+            List<RibbonPanel> panels = application.GetRibbonPanels(tabName)
+                .Where(i => i.Name == "Армирование")
+                .ToList();
+            if (panels.Count == 1)
+            {
+                panel = panels.First();
+            }
+            else
+            {
+                panel = application.CreateRibbonPanel(tabName, "Армирование");
+            }
+
+            PushButton btnAllRebarAsBody = panel.AddItem(new PushButtonData(
+                "ExplodeRebarSer",
+                "Взорвать",
+                assemblyPath,
+                "ExplodeRebarSet.CommandExplode")
+                ) as PushButton;
+
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+
+    }
+}
---
+++ ExplodeRebarSet/CommandExplode.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+
+namespace ExplodeRebarSet
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class CommandExplode : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            Selection sel = commandData.Application.ActiveUIDocument.Selection;
+            if(sel.GetElementIds().Count == 0)
+            {
+                message = "Выберите арматурные стержни";
+                return Result.Failed;
+            }
+
+            Rebar bar = doc.GetElement(sel.GetElementIds().First()) as Rebar;
+            if(bar == null)
+            {
+                message = "Выберите арматурные стержни";
+                return Result.Failed;
+            }
+#if R2017
+            XYZ normal = bar.Normal;
+#else
+            RebarShapeDrivenAccessor acc = bar.GetShapeDrivenAccessor();
+            XYZ normal = acc.Normal;
+#endif
+            RebarBarType barType = doc.GetElement(bar.GetTypeId()) as RebarBarType;
+
+            int rebarStyleNumber = bar.get_Parameter(BuiltInParameter.REBAR_ELEM_HOOK_STYLE).AsInteger();
+            RebarStyle rebarStyle = (RebarStyle)rebarStyleNumber;
+
+            RebarHookType hookTypeStart = null;
+            ElementId hookStartTypeId = bar.get_Parameter(BuiltInParameter.REBAR_ELEM_HOOK_START_TYPE).AsElementId();
+            if (hookStartTypeId != null)
+            {
+                hookTypeStart = doc.GetElement(hookStartTypeId) as RebarHookType;
+            }
+
+            RebarHookType hookTypeEnd = null;
+            ElementId hookEndTypeId = bar.get_Parameter(BuiltInParameter.REBAR_ELEM_HOOK_END_TYPE).AsElementId();
+            if (hookEndTypeId != null)
+            {
+                hookTypeEnd = doc.GetElement(hookEndTypeId) as RebarHookType;
+            }
+
+
+            RebarBendData rbd = bar.GetBendData();
+            RebarHookOrientation hookOrient0 = rbd.HookOrient0;
+            RebarHookOrientation hookOrient1 = rbd.HookOrient1;
+
+            Element host = doc.GetElement(bar.GetHostId());
+
+            List<Curve> curves = bar.GetCenterlineCurves(false, true, true, MultiplanarOption.IncludeOnlyPlanarCurves, 0).ToList();
+            int barsCount = bar.NumberOfBarPositions;
+
+            List<ElementId> newRebarIds = new List<ElementId>();
+            using (Transaction tr = new Transaction(doc))
+            {
+                tr.Start("Explode rebar set");
+                for (int i = 0; i < barsCount; i++)
+                {
+#if R2017
+                    Transform barOffset = bar.GetBarPositionTransform(i);
+#else
+                    Transform barOffset = acc.GetBarPositionTransform(i);
+#endif
+                    XYZ offset = barOffset.Origin;
+
+                    Rebar newRebar = Rebar.CreateFromCurves(doc, rebarStyle, barType, hookTypeStart, hookTypeEnd, host, normal, curves,
+                                                            hookOrient0, hookOrient1, true, false);
+                    doc.Regenerate();
+                    ElementTransformUtils.MoveElement(doc, newRebar.Id, offset);
+                    newRebarIds.Add(newRebar.Id);
+                }
+
+                doc.Delete(bar.Id);
+
+                tr.Commit();
+            }
+
+            sel.SetElementIds(newRebarIds);
+
+            return Result.Succeeded;
+
+        }
+    }
+}
